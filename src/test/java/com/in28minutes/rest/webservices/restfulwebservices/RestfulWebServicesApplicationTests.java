cbbd8837d5aa96fd15ad5fd42a65a19e2e8a885b
grananayak <57844823+grananayak@users.noreply.github.com>
2021-10-12 20:50:15 +0530

    Update RestfulWebServicesApplicationTests.java

--- src/test/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
+++ src/test/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -5,7 +5,6 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.junit.*;
-import org.springframework.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
